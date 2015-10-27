fef2f9083109330438d6b06de0b698caf5bae75f
Aman Jain <amankj@gmail.com>
2015-10-26 22:46:42 -0500

    Added collection2 for validating schemas

--- iss-test.jsx
+++ iss-test.jsx
@@ -1,6 +1,39 @@
 // Define a collection to hold our tasks
 Firms = new Mongo.Collection("firms");
 
+var Schemas = {};
+
+Schemas.Firm = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Name",
+        max: 40
+    // },
+    // author: {
+    //     type: String,
+    //     label: "Author"
+    // },
+    // copies: {
+    //     type: Number,
+    //     label: "Number of copies",
+    //     min: 0
+    // },
+    // lastCheckedOut: {
+    //     type: Date,
+    //     label: "Last date this book was checked out",
+    //     optional: true
+    // },
+    // summary: {
+    //     type: String,
+    //     label: "Brief summary",
+    //     optional: true,
+    //     max: 1000
+    }
+});
+
+//attach SimpleSchema to Firms
+Firms.attachSchema(Schemas.Book);
+
 if (Meteor.isClient) {
   // This code is executed on the client only
 
